5dd443e512f9c0f38e2e1bc4948fcd7a6b62822a
mansi0xc <mansi.tibrewal76@gmail.com>
2025-02-22 08:19:00 +0000

    nft minting contract

--- web3/src/bloodCamp/BloodCamp.sol
+++ web3/src/bloodCamp/BloodCamp.sol
@@ -1,6 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BloodCampNFT is ERC721URIStorage, Ownable {
+    uint256 private _tokenIds;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC721("BloodDonationNFT", "BDNFT") {}
+
+    function mintNFT(address recipient, string memory tokenURI) external onlyOwner returns (uint256) {
+        _tokenIds++;
+        uint256 newItemId = _tokenIds;
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}
+
 contract BloodCamp {
     struct camp {
         uint256 id;
